0159c67efc5a742b99581e5607c54efaf5d56e29
zohrath <zohrath@gmail.com>
2017-09-20 20:03:13 +0200

    getedgecost har nu Ggit add getEdgeCostTest.R git add getEdgeCostTest.R git add getEdgeCostTest.R

--- Assignments/Ass. 1/Solution/getEdgeCostTest.R
+++ Assignments/Ass. 1/Solution/getEdgeCostTest.R
@@ -7,14 +7,14 @@ testMiddlePos = c(c(3,8), c(2,9), c(5,5))
 
 currentPos = c(1,10)
 neighbours = list(c(1,9), c(2,10))
-h = c()
+g = c()
 print("hroads is:")
 print(hroads)
 print("vroadsh is:")
 print(vroads)
 print("currentPos is:")
 print(currentPos)
-print("det nya")
+
 for (i in neighbours) {
   
   print("i is:")
@@ -22,9 +22,9 @@ for (i in neighbours) {
   # Neighbour to the right
   if (i[1] > currentPos[1] & i[2] == currentPos[2]) {
     print("Neighbour to right")
-    h = c(h, hroads[currentPos[2], currentPos[1]+1])
-    print("h:")
-    print(h)
+    g = c(g, hroads[currentPos[2], currentPos[1]+1])
+    print("g:")
+    print(g)
     
   }
   
@@ -32,27 +32,27 @@ for (i in neighbours) {
   if (i[1] < currentPos[1] & i[2] == currentPos[2]) {
     print("Neighbour to left")
     cat(sprintf("hroads[1] and [2]: %d\n", (hroads[currentPos[1], currentPos[2]]) ))
-    h = c(h, hroads[currentPos[2], currentPos[1]-1])
-    print("h:")
-    print(h)
+    g = c(g, hroads[currentPos[2], currentPos[1]-1])
+    print("g:")
+    print(g)
     
   }
   
   # Neighbour above
   if (i[2] > currentPos[2] & i[1] == currentPos[1]) {
     print("Neighbour above")
-    h = c(h, vroads[currentPos[2]+1, currentPos[1]])
-    print("h:")
-    print(h)
+    g = c(g, vroads[currentPos[2]+1, currentPos[1]])
+    print("g:")
+    print(g)
     
   }
   
   # Neighbour below
   if (i[2] < currentPos[2] & i[1] == currentPos[1]) {
     print("Neighbour below")
-    h = c(h, vroads[currentPos[2]-1, currentPos[1]])
-    print("h:")
-    print(h)
+    g = c(g, vroads[currentPos[2]-1, currentPos[1]])
+    print("g:")
+    print(g)
   }
 }
 
