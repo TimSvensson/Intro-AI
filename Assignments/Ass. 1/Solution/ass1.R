d4f4b91708c68d0f2a5e6e830f8040db9e857e06
nurre <nurre13@gmail.com>
2017-09-22 18:28:22 +0200

    Started developing search with data frames

--- Assignments/Ass. 1/Solution/ass1.R
+++ Assignments/Ass. 1/Solution/ass1.R
@@ -415,8 +415,65 @@ getNextMove = function(carPos, destPos, roads) {
     return(nextMove)
 }
 
-getEdgeCost = function(neighbours, currPos) {
+getEdgeCost = function(neighbours, currentPos) {
+  hroads = roads$hroads
+  vroads = roads$vroads
+  
+  h = c()
+  print("hroads is:")
+  print(hroads)
+  print("vroadsh is:")
+  print(vroads)
+  print("currentPos is:")
+  print(currentPos)
+  print("det nya")
+  for (i in neighbours) {
     
+    print("i is:")
+    print(i)
+    # Neighbour to the right
+    if (i[1] > currentPos[1] & i[2] == currentPos[2]) {
+      print("Neighbour to right")
+      h = c(h, hroads[currentPos[2], currentPos[1]+1])
+      print("h:")
+      print(h)
+      
+    }
+    
+    # Neighbour to the left
+    if (i[1] < currentPos[1] & i[2] == currentPos[2]) {
+      print("Neighbour to left")
+      cat(sprintf("hroads[1] and [2]: %d\n", (hroads[currentPos[1], currentPos[2]]) ))
+      h = c(h, hroads[currentPos[2], currentPos[1]-1])
+      print("h:")
+      print(h)
+      
+    }
+    
+    # Neighbour above
+    if (i[2] > currentPos[2] & i[1] == currentPos[1]) {
+      print("Neighbour above")
+      h = c(h, vroads[currentPos[2]+1, currentPos[1]])
+      print("h:")
+      print(h)
+      
+    }
+    
+    # Neighbour below
+    if (i[2] < currentPos[2] & i[1] == currentPos[1]) {
+      print("Neighbour below")
+      h = c(h, vroads[currentPos[2]-1, currentPos[1]])
+      print("h:")
+      print(h)
+    }
+  }
+  
+  
+  
+  
+  # If x or y value > currentPos then move right or up
+  # If x or y value < currentPos then move left or down
+  
 }
 
 getNeighbors = function(currPos, roads) {
